fc5c3a60121020047db4fc55f65d8075897f1d4a
FabianCN22 <facamposn@gmail.com>
2026-06-29 03:02:02 +0000

    Jun 28, 2026, 9:02 PM

--- src/lista_simple.c
+++ src/lista_simple.c
@@ -5,14 +5,11 @@ NodoSimple *ls_crear_nodo(int dato) {
     NodoSimple *nuevo;
 
     nuevo = (NodoSimple *) malloc(sizeof(NodoSimple));
-
     if (nuevo == NULL) {
         return NULL;
     }
-
     nuevo->dato = dato;
     nuevo->sig = NULL;
-
     return nuevo;
 }
 
@@ -22,16 +19,12 @@ int ls_insertar_inicio(NodoSimple **lista, int dato) {
     if (lista == NULL) {
         return 0;
     }
-
     nuevo = ls_crear_nodo(dato);
-
     if (nuevo == NULL) {
         return 0;
     }
-
     nuevo->sig = *lista;
     *lista = nuevo;
-
     return 1;
 }
 
@@ -42,26 +35,19 @@ int ls_insertar_final(NodoSimple **lista, int dato) {
     if (lista == NULL) {
         return 0;
     }
-
     nuevo = ls_crear_nodo(dato);
-
     if (nuevo == NULL) {
         return 0;
     }
-
     if (*lista == NULL) {
         *lista = nuevo;
         return 1;
     }
-
     actual = *lista;
-
     while (actual->sig != NULL) {
         actual = actual->sig;
     }
-
     actual->sig = nuevo;
-
     return 1;
 }
 
@@ -69,35 +55,26 @@ int ls_insertar_pos(NodoSimple **lista, int dato, int pos) {
     NodoSimple *nuevo;
     NodoSimple *actual;
     int i;
-
     if (lista == NULL || pos < 0) {
         return 0;
     }
-
     if (pos == 0) {
         return ls_insertar_inicio(lista, dato);
     }
-
     actual = *lista;
     i = 0;
-
     while (actual != NULL && i < pos - 1) {
         actual = actual->sig;
         i++;
     }
-
     if (actual == NULL) {
         return 0;
     }
-
     nuevo = ls_crear_nodo(dato);
-
     if (nuevo == NULL) {
         return 0;
     }
-
     nuevo->sig = actual->sig;
     actual->sig = nuevo;
-
     return 1;
 }
